f43cee3703a6c614c03c8c4aa74b9ead54a364a2
Paul Ayuk <ayukpaul1@gmail.com>
2024-10-13 19:39:48 +0300

    views

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,12 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Messages') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Settings') }}
+                    </x-nav-link>
                 </div>
             </div>
 
