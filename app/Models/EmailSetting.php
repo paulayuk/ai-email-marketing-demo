2780107815197ac8afaa76ca9401b0c5724b0e7c
Paul Ayuk <ayukpaul1@gmail.com>
2024-10-14 17:03:35 +0300

    added migration and model code

--- app/Models/EmailSetting.php
+++ app/Models/EmailSetting.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class EmailSetting extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'live_key',
+        'webhook_url',
+    ];
 }
--- database/migrations/2024_10_14_135920_create_email_settings_table.php
+++ database/migrations/2024_10_14_135920_create_email_settings_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('email_settings', function (Blueprint $table) {
             $table->id();
+            $table->string('user_id');
+            $table->string('live_key');
+            $table->string('webhook_url');
             $table->timestamps();
         });
     }
