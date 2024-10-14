5779d5cef309cc61ab2b15ad70595625de4866e9
Paul Ayuk <ayukpaul1@gmail.com>
2024-10-14 12:35:23 +0300

    page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/messages', function () {
+    return view('messages');
+})->middleware(['auth', 'verified'])->name('messages');
+
+Route::get('/contacts', function () {
+    return view('contacts');
+})->middleware(['auth', 'verified'])->name('contacts');
+
+Route::get('/settings', function () {
+    return view('settings');
+})->middleware(['auth', 'verified'])->name('settings');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
