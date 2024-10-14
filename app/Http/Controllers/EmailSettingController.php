93458d63ec7e65bffa2a8057fcc814656ea61a1e
Paul Ayuk <ayukpaul1@gmail.com>
2024-10-14 18:06:56 +0300

    add

--- app/Http/Controllers/EmailSettingController.php
+++ app/Http/Controllers/EmailSettingController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\EmailSetting;
 use Illuminate\Http\Request;
 
 class EmailSettingController extends Controller
 {
-    //
+    public function __invoke(Request $request){
+
+    }
 }
+
+
+                $provider->addKeys(
+                        $request->user()->{'id'},
+                        $request->input('api_key'),
+                    )->{'webhook_url'},
+
+        EmailSetting::create([
+            'user_id' => $company['id'],
+            'api_key' => $isDefault,
+            'webhook_url' => config('app.url')."/api/webhooks",
+        ]);
