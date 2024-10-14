69d3436f706b1fab61f7b1592f5c998bd6dc37b4
Paul Ayuk <ayukpaul1@gmail.com>
2024-10-14 16:02:42 +0300

    settings page

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -6,10 +6,18 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    Settings
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
+                        @csrf
+                        @method('post')
+
+                        <div>
+                            <x-input-label for="your_api_key" :value="__('Your secret API Key')" />
+                            <x-text-input id="your_api_key" name="api_key" type="text" class="mt-1 block w-full" />
+
+                        </div>
                 </div>
             </div>
         </div>
